ad96dfe500b1e40c9e641f1bddc8005e81f3b02c
Isen K <isenkasa1005@gmail.com>
2022-10-18 20:07:59 -0500

    Arrays - contains duplicate solution

--- backend/routers/arrays.py
+++ backend/routers/arrays.py
@@ -8,6 +8,22 @@ router = APIRouter(
     }
 )
 
+def string_to_arr(arr):
+    return list(map(int, arr.split(",")))
+
 @router.get("/", tags=["arrays"])
 async def arrays_api():
     return {"description": "Arrays API"}
+
+@router.get("/contains-duplicate/")
+async def contains_duplicate(arr: str):
+    arr = string_to_arr(arr)
+
+    hashset = set()
+
+    for n in arr:
+        if n in hashset:
+            return True
+        hashset.add(n)
+
+    return False
