e66516503f6048a9a254de0d5fb97c69966e61be
Isen K <isenkasa1005@gmail.com>
2022-10-26 16:36:45 -0500

    frontent initialized

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <app-header/>
+  <router-view/>
+  <app-footer/>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+}
+
+nav {
+  a {
+    font-weight: bold;
+    color: #042861;
+
+    &.router-link-exact-active {
+      color: #363636;
+    }
+  }
+}
+</style>
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p> This application contains helpful Data Structure and Algorithm tools. It was built with a purpose to glue together practical skills, such as Full-Stack web development with theoretical skills - DSA.</p>
+  </div>
+</template>
